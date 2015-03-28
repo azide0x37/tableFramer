2647581a3ae26c5e1a5fc8c3119da6586cc416ee
Alexander Templeton <azide0x37@users.noreply.github.com>
2015-03-28 17:42:16 -0500

    trying to set up as a single listComp

--- tableFramer.py
+++ tableFramer.py
@@ -4,32 +4,32 @@ from collections import OrderedDict
 from datetime import datetime 
 
 class tableFramer:
-  def __init__(self, url, dataFormat = dataframe):
-    self.url = url
-    self.dataFormat = dataFormat
+    def __init__(self, url, dataFormat = dataframe):
+        self.url = url
+        self.dataFormat = dataFormat
     
-  def __call__(self):
-    allTable = webpageSouped.findAll('table')
-    [x.findAll('td') for x in headingTable]
-    allTableData = [y.findAll('tr') for y in [x.findAll('td') for x in headingTable]]
+    def __call__(self):
+
+        allTableData = [{y.findAll('tr'):x} for y in x.findAll('td') for x in webpageSouped.findAll('table')]
     
-    dataTable = webpageSouped.find('table', border="0", cellpadding="4", cellspacing="2")
-    rows = dataTable.findAll('tr')
+        heading = [x.findAll('tr') for x in webpageSouped.findAll('table')]
+        data = [y.findall('td') for y in heading]
     
-    dataset = []
-    
-    for tr in rows:
-      cols = OrderedDict()
-      counter = 0
+        dataset = {x:y for x in heading for y in data}
+        
+        for tr in rows:
+            cols = OrderedDict()
+            counter = 0
     .  
-      for td in cols[1:]:
-        text = ''.join(td.find(text=True))
-        try:
-          headings[counter] = text
-          counter += 1
-        except:
-          counter = 0
-          continue
-      dataset.append(row_data)
+        for td in cols[1:]:
+            text = ''.join(td.find(text=True))
+            try:
+                headings[counter] = text
+                counter += 1
+            except:
+                counter = 0
+                continue
+            
+            dataset.append(row_data)
       
-    return pd.DataFrame(dataset)
+        return pd.DataFrame(dataset)
