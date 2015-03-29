21d15f35ba24fd74cf2df09634c5320e52dae894
Alexander Templeton <blakmatr@gmail.com>
2015-03-29 03:35:11 -0500

    So very broken. Formatting. Structural errors.

--- tableFramer.py
+++ tableFramer.py
@@ -1,5 +1,7 @@
 import urllib2 
+import json
 import pandas as pd 
+from bs4 import BeautifulSoup
 from collections import OrderedDict 
 
 class tableFramer:
@@ -12,13 +14,14 @@ class tableFramer:
         self.response = opener.open(self.url)
     
     def __call__(self):
-
-        table_data = [[cell.text for cell in row("td")] for row in BeautifulSoup(self.response.read())("tr")]
+        souped = BeautifulSoup(self.response.read())
+        tables = souped.findAll('table')
+        table_data = [[cell.text for cell in row("td")] for row in tables]
         
         if self.dataFormat.lower() == 'json':
             return json.dumps(OrderedDict(table_data))
         else:
             return pd.DataFrame(table_data)
 
-copeData = tableFramer('https://web.mo.gov/doc/offSearchWeb/searchOffender.do?docId=512073','json')
+copeData = tableFramer('https://web.mo.gov/doc/offSearchWeb/searchOffender.do?docId=512073')
 print copeData()
