9ef56c466948030a262b44b83ae80743a7821adf
Alexander Templeton <blakmatr@gmail.com>
2015-03-31 04:06:53 -0500

    table headings negative.

--- tableFramer.py
+++ tableFramer.py
@@ -1,44 +1,42 @@
 import requests
-import json
-import pandas as pd 
+import json 
 from bs4 import BeautifulSoup
 from collections import OrderedDict 
 
 class tableFramer:
-    def __init__(self, url, dataFormat = 'dataframe'):
+    def __init__(self, url):
         self.url = url
-        self.dataFormat = dataFormat.lower()
-        self.headers = {'User-Agent': 'Mozilla/5.0'}
-        self.response = requests.get(url, headers=self.headers)
+        self.response = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
 
     def __call__(self):
         souped = BeautifulSoup(self.response.text)
         
-        headerTable = souped.find('thead')
-        colNames = headerTable.findAll('th')
-        table = souped.find('table', summary="Table listing details of the accident.")
-        rows = table.findAll('tr')
-        dataset = []
+        tableHead = souped.find('thead')
+        colNames = tableHead.findAll('th')
+        print "colNames", colNames
+
+        table = souped.find('table', summary = "Table listing details of the accident.")
+        rows = table.findAll('tr', class_ = "infoCell")
+        print "rows", rows
         
+        dataset = []
+
         for tr in rows:
             cols = tr.findAll('td')
-            row_data = OrderedDict()
-            counter = 0
+            rowData = OrderedDict()
+            counter = 1
 
             for td in cols[1:]:
                 text = ''.join(td.find(text=True))
                 try:
-                    row_data[self.colNames[counter]] = text
+                    rowData[colNames[counter]] = text
                     counter += 1
                 except:
                     counter = 0
                     continue
-            dataset.append(row_data)
+            dataset.append(rowData)
 
-        if self.dataFormat != 'dataframe':
-            return json.dumps(dataset, indent=4, separators=(',',':'))
-        else:
-            return pd.DataFrame(table_data)
-
-copeData = tableFramer('http://www.mshp.dps.missouri.gov/HP68/SearchAction','json')
-print copeData()
+        return json.dumps(dataset, indent=4, separators=(',',':'))
+        
+crashData = tableFramer('http://www.mshp.dps.missouri.gov/HP68/SearchAction')
+print crashData()
